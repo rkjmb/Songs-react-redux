69bb3f965144d36a43a23b809abee5ffbea02711
rkjmb <rkjmb123@gmail.com>
2019-10-25 20:09:17 +0530

    Formatting

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import SongList from './SongList';
-import SongDetail from './SongDetail';
+import React from "react";
+import SongList from "./SongList";
+import SongDetail from "./SongDetail";
 
-const App=()=>{
-    return (
-    <div className="ui container grid"><div className="ui row"><div className="column eight wide">
-    <SongList/>
-    <div className="column eight wide">
-    <SongDetail/>
+const App = () => {
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+          <div className="column eight wide">
+            <SongDetail />
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-    </div>
-    
-    )
-}
+  );
+};
 
 export default App;
